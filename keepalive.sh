b1dcb7a532d6bcec82b016c1882a3696ec598748
Z User <z@container>
2026-04-17 13:13:32 +0000

    c584ce92-72ea-45e7-8509-04610446429c

--- keepalive.sh
+++ keepalive.sh
@@ -1,8 +1,13 @@
-#!/bin/bash
+#!/usr/bin/env bash
+# Flux Stream - permanent dev server with auto-restart
+# Runs Next.js dev server and automatically restarts if it dies
+
 cd /home/z/my-project
+
 while true; do
-  echo "[$(date)] Starting dev server..."
-  NODE_OPTIONS="--max-old-space-size=512" npx next dev -p 3000 >> dev.log 2>&1
-  echo "[$(date)] Server exited, restarting in 3s..."
-  sleep 3
+  echo "[$(date '+%H:%M:%S')] Starting Flux Stream dev server..." >> /tmp/flux-keepalive.log
+  npx next dev --port 3000 >> /tmp/next-dev.log 2>&1
+  EXIT_CODE=$?
+  echo "[$(date '+%H:%M:%S')] Server exited with code $EXIT_CODE, restarting in 2s..." >> /tmp/flux-keepalive.log
+  sleep 2
 done
