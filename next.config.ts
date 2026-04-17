78cb2e85483001fff274df8187820da1974801dc
Z User <z@container>
2026-04-17 12:26:47 +0000

    e9a03b66-0b22-4cac-a7c6-aa62ec19c79d

--- next.config.ts
+++ next.config.ts
@@ -2,14 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
-  /* config options here */
   typescript: {
     ignoreBuildErrors: true,
   },
   reactStrictMode: false,
-  allowedDevOrigins: [
-    "https://preview-chat-5b84e09d-9399-4663-ab23-ab9b6a142eb6.space.z.ai",
-  ],
+  allowedDevOrigins: ["*"],
   images: {
     remotePatterns: [
       {
